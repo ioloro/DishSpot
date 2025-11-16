6421e5cf903df65fb2075d812389d219db089f3e
John Marc <johnmarcjohnmarc@me.com>
2025-11-16 06:31:42 -0700

    Initial Commit

---
+++ DishSpot/DishSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishSpotApp.swift
+//  DishSpot
+//
+//  Created by John Marc on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct DishSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
